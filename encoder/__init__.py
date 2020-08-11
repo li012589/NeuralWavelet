301d86eae9a581786dc3d50ea58655862c3b42ee
lish <contactlish@gmail.com>
2020-08-11 10:45:41 +0800

    add encoder and its test template

---
+++ encoder/__init__.py
@@ -0,0 +1,1 @@
+from .rans import *
---
+++ test/test_rans.py
@@ -0,0 +1,10 @@
+import encoder
+
+
+def test_encodeDecode():
+    #encode/decoder a MNIST image using its own distribution
+    pass
+
+def test_encodeDecode_otherDistr():
+    #encode/decoder a MNIST image using OTHER distribution
+    pass
