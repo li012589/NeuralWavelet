6ae0df9ff213e9d3c0b7c59b0e8f91018a68540e
lish <contactlish@gmail.com>
2020-08-26 01:04:18 +0800

    minor fix

--- main.py
+++ main.py
@@ -95,12 +95,15 @@ else:
     raise Exception("No such target")
 
 # Building the sub-priors
+# TODO: depth less than int(math.log(blockLength, 2))
 priorList = []
 _length = int((blockLength * blockLength) / 4)
 for n in range(int(math.log(blockLength, 2))):
     if n != (int(math.log(blockLength, 2))) - 1:
+        # intermedia variable prior, 3 here means the left 3 variable
         priorList.append(source.DiscreteLogistic([channel, _length, 3], decimal, rounding))
     else:
+        # final variable prior, all 4 variable
         priorList.append(source.MixtureDiscreteLogistic([channel, _length, 4], nMixing, decimal, rounding))
     _length = int(_length / 4)
 
@@ -109,6 +112,8 @@ p = source.HierarchyPrior(channel, blockLength, priorList)
 
 # Building NICE model inside MERA
 assert nNICE % 2 == 0
+assert depth <= int(math.log(blockLength, 2))
+
 layerList = []
 if depth == -1:
     depth = None
