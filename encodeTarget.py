fd48ba72f7df28275759a4b861fe95500897806f
lish <contactlish@gmail.com>
2020-09-07 11:13:47 +0800

    update

--- encodeTarget.py
+++ encodeTarget.py
@@ -120,7 +120,36 @@ loadedF = torch.load(name, map_location=device)
 layerList = loadedF.layerList[:(repeat + 1)]
 layerList = [layerList[no] for no in range(repeat + 1)]
 
-p = loadedF.prior
+_pbase = int(math.log(blockLength, 2)) - len(loadedF.prior.priorList)
+plen = list(range(_pbase, int(math.log(blockLength, 2))))
+baselen = list(range(_pbase))
+
+# Building the sub-priors
+# TODO: depth less than int(math.log(blockLength, 2))
+priorList = []
+_length = int((blockLength * blockLength) / 4)
+for n in range(int(math.log(blockLength, 2))):
+    if n in plen:
+        priorList.append(loadedF.prior.priorList[n - _pbase])
+        _length = int(_length / 4)
+        continue
+    if n != (int(math.log(blockLength, 2))) - 1:
+        # intermedia variable prior, 3 here means the left 3 variable
+        if smallPrior:
+            priorList.append(source.DiscreteLogistic([channel, 1, 3], decimal, rounding))
+        else:
+            priorList.append(source.DiscreteLogistic([channel, _length, 3], decimal, rounding))
+    elif n == depth - 1:
+        # if depth is specified, the last prior
+        priorList.append(source.MixtureDiscreteLogistic([channel, _length, 4], nMixing, decimal, rounding))
+        break
+    else:
+        # final variable prior, all 4 variable
+        priorList.append(source.MixtureDiscreteLogistic([channel, _length, 4], nMixing, decimal, rounding))
+    _length = int(_length / 4)
+
+# Building the hierarchy prior
+p = source.HierarchyPrior(channel, blockLength, priorList, repeat=repeat)
 
 # Building NICE model inside MERA
 assert nNICE % 2 == 0
@@ -136,21 +165,24 @@ shapeList = [[3, *term.shape] for term in f.prior.factorOutIList]
 
 
 def divide(z):
+    extraPart = []
     parts = []
     for no in range(int(math.log(blockLength, 2))):
         _, zpart = utils.dispatch(f.prior.factorOutIList[no], f.prior.factorOutJList[no], z)
 
-        import pdb
-        pdb.set_trace()
+        if no in baselen:
+            extraPart.append(zpart)
+            continue
+
         if no != int(math.log(blockLength, 2)) - 1:
             zpart = zpart - decimal.forward_(f.prior.priorList[no].mean).reshape(1, *f.prior.priorList[no].mean.shape).int() + args.nbins // 2
         else:
             zpart = zpart - (decimal.forward_(f.prior.priorList[no].mean.permute([1, 2, 3, 0])) * torch.softmax(f.prior.priorList[no].mixing, -1)).sum(-1).reshape(1, *f.prior.priorList[no].mean.shape[1:]).int() + args.nbins // 2
         parts.append(zpart.reshape(zpart.shape[0], -1).int().detach())
-    return torch.cat(parts, -1).numpy()
+    return torch.cat(parts, -1).numpy(), extraPart
 
 
-def join(rcnZ):
+def join(rcnZ, extraPart):
     retZ = torch.zeros(batch, *targetSize).to(device).int()
     for no in range(int(math.log(blockLength, 2))):
         rcnZpart = rcnZ[:, :(np.prod(shapeList[no]))].reshape(rcnZ.shape[0], *shapeList[no])
@@ -160,6 +192,9 @@ def join(rcnZ):
             rcnZpart = rcnZpart + (decimal.forward_(f.prior.priorList[no].mean.permute([1, 2, 3, 0])) * torch.softmax(f.prior.priorList[no].mixing, -1)).sum(-1).reshape(1, *f.prior.priorList[no].mean.shape[1:]).int() - args.nbins // 2
         else:
             rcnZpart = rcnZpart + decimal.forward_(f.prior.priorList[no].mean).reshape(1, *f.prior.priorList[no].mean.shape).int() - args.nbins // 2
+
+        if no in baselen:
+            rcnZpart = extraPart[no]
         retZ = utils.collect(f.prior.factorOutIList[no], f.prior.factorOutJList[no], retZ, rcnZpart.int())
     return retZ
 
@@ -182,6 +217,64 @@ for no, prior in enumerate(f.prior.priorList):
 CDF = torch.cat(CDF, -1).numpy()
 
 
+def partCoder(part, states):
+    HISTos = []
+    MEANs = []
+    theoryBPD = []
+    CDFs = []
+
+    for i in range(part.shape[0]):
+        mean = int(0.5 * (part[i].max() + part[i].min()))
+        MEANs.append(mean)
+
+        histogram = torch.histc(part[i] - mean, bins=args.nbins, min=-args.nbins // 2, max=args.nbins // 2)
+        HISTos.append(histogram)
+
+        prob = histogram / np.prod(part[i].shape)
+        logp = -torch.log2(prob)
+        logp[logp == float('inf')] = 0
+        theorySize = (logp * histogram).sum().item()
+        theoryBPD.append(theorySize / np.prod(part.shape[1:]))
+
+        cdf = prob.detach().numpy()
+        for j in range(prob.shape[0] - 1):
+            cdf[j + 1] = cdf[j] + cdf[j + 1]
+
+        # np.arange(nBins)).reshape(1, nBins)) here to avoid zero freq in ans, add extra BPD
+        CDFs.append(torch.from_numpy((cdf * ((1 << args.precision))).astype('int').reshape(1, args.nbins)))
+
+    MEANs = torch.tensor(MEANs).reshape(part.shape[0], 1)
+    CDFs = torch.cat(CDFs, 0).numpy()
+
+    print("theory BPD:", np.mean(theoryBPD))
+
+    for i in range(part.shape[0]):
+        symbols = part[i] - MEANs[i] + args.nbins // 2 - 1
+        symbols = symbols.reshape(-1).int()
+        state = states[i]
+        for j in reversed(range(len(symbols))):
+            state = coder.encoder(CDFs[i], symbols[j], state)
+        state = rans.flatten(state)
+        states[i] = state
+    #aBPD = np.mean([32 * len(term) / np.prod(part.shape[1:]) for term in states])
+    #print("actual BPD:", aBPD)
+
+    return states, MEANs, CDFs
+
+
+def partDecode(shape, MEANs, CDFs, states):
+    after_states = []
+    reconstruction = [[] for i in range(len(states))]
+    for i in range(len(states)):
+        state = rans.unflatten(states[i])
+        for j in range(np.prod(shape)):
+            state, recon_symbol = coder.decoder(CDFs[i], state)
+            reconstruction[i].append(recon_symbol + MEANs[i] - args.nbins // 2 + 1)
+        after_states.append(state)
+    reconstruction = torch.tensor(reconstruction).reshape(len(states), *shape)
+    return reconstruction, after_states
+
+
 def testBPD(loader, earlyStop=-1):
     actualBPD = []
     theoryBPD = []
@@ -192,7 +285,7 @@ def testBPD(loader, earlyStop=-1):
         count += 1
         z, _ = f.inverse(samples)
 
-        zparts = divide(z)
+        zparts, extras = divide(z)
 
         state = []
         for i in range(batch):
@@ -203,12 +296,39 @@ def testBPD(loader, earlyStop=-1):
                 s = coder.encoder(cdf, symbols[j], s)
             state.append(rans.flatten(s))
 
-        actualBPD.append(32 / (3 * 32 * 32) * np.mean([s.shape[0] for s in state]))
-        theoryBPD.append((-f.logProbability(samples).mean() / (np.prod(samples.shape[1:]) * np.log(2.))).detach().item())
+        '''
+        from copy import copy, deepcopy
+        backState = copy(state)
+        #state = [rans.x_init for _ in range(samples.shape[0])]
+
+        SHAPEs = []
+        MEANs = []
+        CDFs = []
+        for extra in extras:
+            SHAPEs.append(extra.shape[1:])
+            state, means, cdfs = partCoder(extra, state)
+            MEANs.append(means)
+            CDFs.append(cdfs)
+
+        actualBPD.append(32 / (np.prod(samples.shape[1:])) * np.mean([s.shape[0] for s in state]))
+        print("actualBPD:", actualBPD)
+        #theoryBPD.append((-f.logProbability(samples).mean() / (np.prod(samples.shape[1:]) * np.log(2.))).detach().item())
+        RCNExtraPart = []
+        for t in range(len(SHAPEs)):
+            rcnExtras, state = partDecode(SHAPEs[t], MEANs[t], CDFs[t], state)
+            RCNExtraPart.append(rcnExtras)
+
+        for no in range(200):
+            assert (state[no] == backState[no] and id(state[no]) != id(backState[no]))
+
+        import pdb
+        pdb.set_trace()
+        '''
+        #state = [rans.flatten(s) for s in state]
 
         rcnParts = []
         for i in range(batch):
-            s = rans.unflatten(state[i])
+            s = state[i]
             symbols = []
             for j in range(np.prod(targetSize)):
                 cdf = CDF[:, j]
@@ -216,6 +336,9 @@ def testBPD(loader, earlyStop=-1):
                 symbols.append(rcnSymbol)
             rcnParts.append(torch.tensor(symbols).reshape(1, -1))
         rcnParts = torch.cat(rcnParts, 0)
+
+        import pdb
+        pdb.set_trace()
 
         rcnZ = join(rcnParts)
 
