0fd4f20962c0b30b3bcb2bdb036932c805acdb2a
lish <contactlish@gmail.com>
2020-09-07 16:26:27 +0800

    minor fix

--- progressiveLoading.py
+++ progressiveLoading.py
@@ -65,7 +65,7 @@ if target == "CIFAR":
     testTarget = torchvision.datasets.CIFAR10(root='./data/cifar', train=False, download=True, transform=trainsetTransform)
     targetTrainLoader = torch.utils.data.DataLoader(trainTarget, batch_size=batch, shuffle=True)
     targetTestLoader = torch.utils.data.DataLoader(testTarget, batch_size=batch, shuffle=True)
-elif args.target == "ImageNet32":
+elif target == "ImageNet32":
     # Define dimensions
     targetSize = [3, 32, 32]
     dimensional = 2
@@ -84,7 +84,7 @@ elif args.target == "ImageNet32":
     targetTrainLoader = torch.utils.data.DataLoader(trainTarget, batch_size=batch, shuffle=True)
     targetTestLoader = torch.utils.data.DataLoader(testTarget, batch_size=batch, shuffle=True)
 
-elif args.target == "ImageNet64":
+elif target == "ImageNet64":
     # Define dimensions
     targetSize = [3, 64, 64]
     dimensional = 2
@@ -103,7 +103,7 @@ elif args.target == "ImageNet64":
     targetTrainLoader = torch.utils.data.DataLoader(trainTarget, batch_size=batch, shuffle=True)
     targetTestLoader = torch.utils.data.DataLoader(testTarget, batch_size=batch, shuffle=True)
 
-elif args.target == "MNIST":
+elif target == "MNIST":
     pass
 else:
     raise Exception("No such target")
