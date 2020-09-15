4de225cb81616936d2b706df1c12914836c39f87
lish <contactlish@gmail.com>
2020-09-15 17:45:31 +0800

    add test for non-samplable flow

--- test/flowRelated.py
+++ test/flowRelated.py
@@ -7,8 +7,11 @@ import sys
 sys.path.append(os.getcwd())
 #from utils import assertMTJM,jacobian
 
-def bijective(flow,batch=100,decimal=5):
-    x,p = flow.sample(batch)
+def bijective(flow,batch=100,decimal=5, sampleMethod=None):
+    if sampleMethod is None:
+        x,p = flow.sample(batch)
+    else:
+        x,p = sampleMethod(batch)
     z,ip = flow.inverse(x)
     xz,gp = flow.forward(z)
     op = flow.prior.logProbability(z)
@@ -17,10 +20,14 @@ def bijective(flow,batch=100,decimal=5):
     assert_array_almost_equal(z.detach().numpy(),zx.detach().numpy(),decimal=decimal)
     assert_array_almost_equal(ip.detach().numpy(),-gp.detach().numpy(),decimal=decimal)
     assert_array_almost_equal(ip.detach().numpy(),ipp.detach().numpy(),decimal=decimal)
-    assert_array_almost_equal(p.detach().numpy(),(op-gp).detach().numpy(),decimal=decimal)
+    if sampleMethod is None:
+        assert_array_almost_equal(p.detach().numpy(),(op-gp).detach().numpy(),decimal=decimal)
 
-def saveload(flow,blankFlow,batch=100,decimal=5):
-    x,p = flow.sample(batch)
+def saveload(flow,blankFlow,batch=100,decimal=5, sampleMethod=None):
+    if sampleMethod is None:
+        x,p = flow.sample(batch)
+    else:
+        x,p = sampleMethod(batch)
     z,ip = flow.inverse(x)
     d = flow.save()
     torch.save(d,"testsaving.saving")
@@ -30,7 +37,8 @@ def saveload(flow,blankFlow,batch=100,decimal=5):
     xz,gp = blankFlow.forward(z)
     assert_array_almost_equal(x.detach().numpy(),xz.detach().numpy(),decimal=decimal)
     assert_array_almost_equal(ip.detach().numpy(),-gp.detach().numpy(),decimal=decimal)
-    assert_array_almost_equal(p.detach().numpy(),(op-gp).detach().numpy(),decimal=decimal)
+    if sampleMethod is None:
+        assert_array_almost_equal(p.detach().numpy(),(op-gp).detach().numpy(),decimal=decimal)
 
 
 def symplectic(flow,batch=100,decimal=5):
