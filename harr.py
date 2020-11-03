bbb7e6a1e07b2da615c6885c33c9c86c0fa566cf
lish <contactlish@gmail.com>
2020-11-03 22:23:39 +0800

    add build fn

--- harr.py
+++ harr.py
@@ -3,7 +3,7 @@ from flow import ScalingNshifting
 
 lineSize = 4
 batch = 2
-hchnl = 5
+hchnl = 10
 
 decimal = ScalingNshifting(256, -128)
 
@@ -24,50 +24,49 @@ transMatrix = torch.tensor([[1/2, 1/2, 0, 0, 0, 0, 0, 0],
 trans_v = torch.matmul(v, transMatrix.t())
 
 originalChnl = 3
-chnlList = [originalChnl, hchnl, hchnl, originalChnl]
-convList = [torch.nn.ReplicationPad1d((0, 2), )]
 
-for no, chnl in enumerate(chnlList[:-1]):
-    if no == 0:
-        layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3)
-        layer(o)
-        tmp = torch.cat([torch.ones(originalChnl, 1), torch.zeros(originalChnl, 2)], 1).reshape(originalChnl, 1, 3)
-        layer.weight.data[:originalChnl, :, :] = torch.cat([tmp, torch.zeros(originalChnl, chnl, 3)], 1).reshape(-1)[:originalChnl * chnl * 3].reshape(originalChnl, chnl, 3).float()
+def initMethod1(originalChnl):
+    return torch.cat([torch.ones(originalChnl, 1), torch.zeros(originalChnl, 2)], 1).reshape(originalChnl, 1, 3)
+
+def initMethod2(originalChnl):
+    return torch.cat([torch.zeros(originalChnl, 2), torch.ones(originalChnl, 1) / 2], 1).reshape(originalChnl, 1, 3)
+
+
+def buildLayers(initMethod, originalChnl, hchnl, nhidden=1, right=True):
+    chnlList = [originalChnl] + [hchnl] * (nhidden + 1) + [originalChnl]
+    if right:
+        convList = [torch.nn.ReplicationPad1d((0, 2), )]
     else:
-        layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3, padding=1, padding_mode="replicate")
-        if no == len(chnlList) - 2:
+        convList = [torch.nn.ReplicationPad1d((2, 0), )]
+
+    for no, chnl in enumerate(chnlList[:-1]):
+        if no == 0:
+            layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3)
+            tmp = initMethod()
+            tmp1 = torch.cat([tmp, torch.zeros(originalChnl, chnl, 3)], 1).reshape(-1)[:originalChnl * chnl * 3].reshape(originalChnl, chnl, 3)
+            tmp2 = -torch.cat([tmp, torch.zeros(originalChnl, chnl, 3)], 1).reshape(-1)[:originalChnl * chnl * 3].reshape(originalChnl, chnl, 3)
+            layer.weight.data[:2 * originalChnl, :, :] = torch.cat([tmp1, tmp2], 0).float()
+        elif no == len(chnlList) - 2:
+            layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3, padding=1, padding_mode="replicate")
             layer.weight.data.zero_()
-        tmp = torch.cat([torch.zeros(originalChnl, 1), torch.ones(originalChnl, 1), torch.zeros(originalChnl, 1)], 1).reshape(originalChnl, 1, 3)
-        layer.weight.data[:originalChnl, :, :] = torch.cat([tmp, torch.zeros(originalChnl, chnl, 3)], 1).reshape(-1)[:originalChnl * chnl * 3].reshape(originalChnl, chnl, 3).float()
-    layer.bias.data[:originalChnl].zero_()
-    convList.append(layer)
-    if no != len(chnlList) - 2:
-        convList.append(torch.nn.Hardtanh(inplace=True))
+            tmp = torch.cat([torch.zeros(originalChnl, 1), torch.ones(originalChnl, 1), torch.zeros(originalChnl, 1)], 1).reshape(originalChnl, 1, 3)
+            tmp1 = torch.cat([tmp, torch.zeros(originalChnl, originalChnl, 3)], 1).reshape(-1)[:originalChnl * originalChnl * 3].reshape(originalChnl, originalChnl, 3)
+            tmp2 = -torch.cat([tmp, torch.zeros(originalChnl, originalChnl, 3)], 1).reshape(-1)[:originalChnl * originalChnl * 3].reshape(originalChnl, originalChnl, 3)
+            layer.weight.data[:originalChnl, :2 * originalChnl, :] = torch.cat([tmp1, tmp2], 1).float()
+        else:
+            layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3, padding=1, padding_mode="replicate")
+            tmp = torch.cat([torch.zeros(2 * originalChnl, 1), torch.ones(2 * originalChnl, 1), torch.zeros(2 * originalChnl, 1)], 1).reshape(2 * originalChnl, 1, 3)
+            layer.weight.data[:2 * originalChnl, :, :] = torch.cat([tmp, torch.zeros(2 * originalChnl, chnl, 3)], 1).reshape(-1)[:2 * originalChnl * chnl * 3].reshape(2 * originalChnl, chnl, 3).float()
+        layer.bias.data[:2 * originalChnl].zero_()
+        convList.append(layer)
+        if no != len(chnlList) - 2:
+            convList.append(torch.nn.ReLU(inplace=True))
 
-layer1 = torch.nn.Sequential(*convList)
+    return torch.nn.Sequential(*convList)
 
-originalChnl = 3
-chnlList = [originalChnl, hchnl, hchnl, originalChnl]
-convList = [torch.nn.ReplicationPad1d((2, 0), )]
+layer1 = buildLayers(lambda: initMethod1(originalChnl), originalChnl, hchnl, 2)
 
-for no, chnl in enumerate(chnlList[:-1]):
-    if no == 0:
-        layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3)
-        layer(o)
-        tmp = torch.cat([torch.zeros(originalChnl, 2), torch.ones(originalChnl, 1) / 2], 1).reshape(originalChnl, 1, 3)
-        layer.weight.data[:originalChnl, :, :] = torch.cat([tmp, torch.zeros(originalChnl, chnl, 3)], 1).reshape(-1)[:originalChnl * chnl * 3].reshape(originalChnl, chnl, 3).float()
-    else:
-        layer = torch.nn.Conv1d(chnl, chnlList[no + 1], 3, padding=1, padding_mode="replicate")
-        if no == len(chnlList) - 2:
-            layer.weight.data.zero_()
-        tmp = torch.cat([torch.zeros(originalChnl, 1), torch.ones(originalChnl, 1), torch.zeros(originalChnl, 1)], 1).reshape(originalChnl, 1, 3)
-        layer.weight.data[:originalChnl, :, :] = torch.cat([tmp, torch.zeros(originalChnl, chnl, 3)], 1).reshape(-1)[:originalChnl * chnl * 3].reshape(originalChnl, chnl, 3).float()
-    layer.bias.data[:originalChnl].zero_()
-    convList.append(layer)
-    if no != len(chnlList) - 2:
-        convList.append(torch.nn.Hardtanh(inplace=True))
-
-layer2 = torch.nn.Sequential(*convList)
+layer2 = buildLayers(lambda: initMethod2(originalChnl), originalChnl, hchnl, 2, False)
 
 d = e - layer1(o)
 
@@ -76,6 +75,8 @@ s = o + layer2(d)
 vpp = torch.cat([s, d], -1)
 
 assert vpp.allclose(trans_v)
+
+exit()
 
 l1 = torch.nn.Conv1d(3, 6, 3)
 l1.bias.data.zero_()
