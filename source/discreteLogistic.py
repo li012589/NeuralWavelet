f1d49d2b69b4659a57665f210a6c39e3084b17c1
lish <contactlish@gmail.com>
2020-11-29 22:37:49 +0800

    change init

--- source/discreteLogistic.py
+++ source/discreteLogistic.py
@@ -39,7 +39,7 @@ class MixtureDiscreteLogistic(Source):
         if mean is None:
             mean = torch.zeros([nMixing] + nvars)
             for i in range(nMixing):
-                mean[i, ...] += i - (nMixing - 1) / 2.
+                mean[i, ...] += (i - (nMixing - 1) / 2.) / 8.
         if logscale is None:
             logscale = torch.zeros([nMixing] + nvars)
 
