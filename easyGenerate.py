dbf64747478c923250838487eb46516f73481667
lish <contactlish@gmail.com>
2020-12-08 11:45:03 +0800

    debug generate/sample api and script

---
+++ easyGenerate.py
@@ -0,0 +1,135 @@
+import numpy as np
+import argparse, json, math
+import os, glob
+
+import flow, utils, source
+
+import torch, torchvision
+from torch import nn
+
+from encoder import rans, coder
+from utils import cdfDiscreteLogitstic, cdfMixDiscreteLogistic
+from matplotlib import pyplot as plt
+
+#torch.manual_seed(42)
+
+parser = argparse.ArgumentParser(description="")
+
+parser.add_argument("-folder", default=None, help="Path to load the trained model")
+parser.add_argument("-cuda", type=int, default=-1, help="Which device to use with -1 standing for CPU, number bigger than -1 is N.O. of GPU.")
+parser.add_argument("-baseScale", type=float, default=-2.0, help="exp scaling of distribution's logscale to achieve better quality")
+parser.add_argument("-best", action='store_false', help="if load the best model")
+parser.add_argument("-num", type=int, default=10, help="num of image to demo")
+
+args = parser.parse_args()
+
+device = torch.device("cpu" if args.cuda < 0 else "cuda:" + str(args.cuda))
+
+if args.folder is None:
+    raise Exception("No loading")
+else:
+    rootFolder = args.folder
+    if rootFolder[-1] != '/':
+        rootFolder += '/'
+    with open(rootFolder + "parameter.json", 'r') as f:
+        config = json.load(f)
+        locals().update(config)
+
+        target = config['target']
+        repeat = config['repeat']
+        nMixing = config['nMixing']
+        batch = config['batch']
+
+# update batch using passing parameter
+batch = args.num ** 2
+
+if target == "CIFAR":
+    # Define dimensions
+    targetSize = [3, 32, 32]
+    dimensional = 2
+    channel = targetSize[0]
+    blockLength = targetSize[-1]
+
+    # Define nomaliziation and decimal
+    decimal = flow.ScalingNshifting(256, -128)
+    rounding = utils.roundingWidentityGradient
+elif target == "ImageNet32":
+    # Define dimensions
+    targetSize = [3, 32, 32]
+    dimensional = 2
+    channel = targetSize[0]
+    blockLength = targetSize[-1]
+
+    # Define nomaliziation and decimal
+    decimal = flow.ScalingNshifting(256, -128)
+    rounding = utils.roundingWidentityGradient
+elif target == "ImageNet64":
+    # Define dimensions
+    targetSize = [3, 64, 64]
+    dimensional = 2
+    channel = targetSize[0]
+    blockLength = targetSize[-1]
+
+    # Define nomaliziation and decimal
+    decimal = flow.ScalingNshifting(256, -128)
+    rounding = utils.roundingWidentityGradient
+
+
+# decide which model to load
+if args.best:
+    name = max(glob.iglob(os.path.join(rootFolder, '*.saving')), key=os.path.getctime)
+else:
+    name = max(glob.iglob(os.path.join(rootFolder, 'savings', '*.saving')), key=os.path.getctime)
+
+# load the model
+print("load saving at " + name)
+loadedF = torch.load(name, map_location=device)
+
+if 'easyMera' in name:
+    layerList = loadedF.layerList[:(4 * repeat)]
+    layerList = [layerList[no] for no in range(4 * repeat)]
+elif '1to2Mera' in name:
+    layerList = loadedF.layerList[:(2 * repeat)]
+    layerList = [layerList[no] for no in range(2 * repeat)]
+else:
+    raise Exception("model not define")
+
+# Define nomaliziation and decimal
+if 'easyMera' in name:
+    decimal = flow.ScalingNshifting(256, -128)
+elif '1to2Mera' in name:
+    decimal = flow.ScalingNshifting(256, 0)
+else:
+    raise Exception("model not define")
+
+if 'simplePrior_False' in name:
+    meanNNlist = [loadedF.meanNNlist[0]]
+    scaleNNlist = [loadedF.scaleNNlist[0]]
+else:
+    meanNNlist = None
+    scaleNNlist = None
+
+rounding = utils.roundingWidentityGradient
+
+prior = loadedF.prior
+
+# Building MERA mode
+if 'easyMera' in name:
+    f = flow.SimpleMERA(blockLength, layerList, meanNNlist, scaleNNlist, repeat, None, nMixing, decimal=decimal, rounding=utils.roundingWidentityGradient).to(device)
+elif '1to2Mera' in name:
+    f = flow.OneToTwoMERA(blockLength, layerList, meanNNlist, scaleNNlist, repeat, None, nMixing, decimal=decimal, rounding=utils.roundingWidentityGradient).to(device)
+
+f.prior = prior
+
+
+samples = f.sample(batch)
+
+samples = samples.int()
+
+grid_img = torchvision.utils.make_grid(samples, nrow=args.num)
+
+plt.figure(figsize=(12, 12))
+
+plt.imshow(grid_img.permute(1, 2, 0))
+
+plt.show()
